729a8049496d4017214cbb5b8f2945c30629ed38
Eric Espinoza <eric@virsix.com>
2015-01-25 21:54:03 -0800

    NOW FEATURING: MOVING TARGETS!!!!!!

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,14 +5,28 @@ public class Target : MonoBehaviour {
 
 	private bool m_hit = false;
 	private Animator m_animator;
+	private Vector3 m_direction;
+	private float m_speed;
 
 	// Use this for initialization
 	void Start () {
 		m_animator = GetComponentInChildren<Animator>();
 	}
 
+	void Update() {
+		transform.Translate( m_direction * m_speed * Time.deltaTime );
+	}
+
 	public void Hit() {
 		m_animator.SetTrigger( "Drop" );
 		m_hit = true;
+	}
+
+	public void SetSpeed( float value ) {
+		m_speed = value;
+	}
+
+	public void SetDirection( Vector3 v ) {
+		m_direction = v;
 	}
 }
---
+++ Assets/TargetSpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetSpawner : MonoBehaviour {
+
+	public GameObject m_baseTarget;
+	public float m_spawnRate = 2f;
+	public float m_targetSpeed = 1f;
+
+	private float timer = 0f;
+	private Transform myTransform;
+	private Transform killBox;
+
+	// Use this for initialization
+	void Start () {
+		myTransform = GetComponent<Transform>();
+		killBox = myTransform.GetChild(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( timer >= m_spawnRate) {
+			GameObject temp = (GameObject)Instantiate( m_baseTarget, myTransform.position, Quaternion.identity );
+			Target tTarget = temp.GetComponentInChildren<Target>();
+			tTarget.SetDirection( (killBox.position - myTransform.position).normalized );
+			tTarget.SetSpeed( m_targetSpeed );
+
+			timer = 0f;
+		}
+
+		timer += Time.deltaTime;
+	}
+}
